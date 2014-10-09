e87b8ba53d3f539ca288853ac87286708f3ccb3f
nertwork <james.farr@dreamhost.com>
2014-10-08 17:36:29 -0700

    5th revision

--- minute_mtr.rb
+++ minute_mtr.rb
@@ -4,7 +4,7 @@ def tstamp
   Time.now.strftime('%H:%M__%m-%d-%y').gsub("\s","")
 end
 while true do
-  mtr = `mtr -b -r -c 1 #{hdest}`
+  mtr = `mtr -n -r -c 1 #{hdest}`
   File.open("#{fsdest}#{hdest}__#{tstamp}.txt", 'w') { |f| f.write(mtr)}
   sleep 60
 end
