9064ba0bd144a39f71c5491cebfe607c360a1b24
nertwork <james.farr@dreamhost.com>
2014-10-09 13:21:55 -0700

    Revision 7 - deleted files

--- minute_mtr.rb
+++ minute_mtr.rb
@@ -1,17 +1,29 @@
 #!/usr/bin/ruby
-hdest,fsdest = ARGV
-if ARGV.empty?
-  puts "Improper usage!!"
-  puts "Usage is minute_mtr.rb DESTINATION FILE_DEST"
-  puts "Example minute_mtr.rb google.com /var/tmp/"
-abort
+unless ARGV.size == 3
+  puts "Usage is minute_mtr.rb <DESTINATION> <FILE_DEST> <MAX DAYS>"
+  puts "Example minute_mtr.rb google.com /var/tmp/ 7"
+  exit 1
 end
-def tstamp
-  Time.now.strftime('%H:%M__%m-%d-%y').gsub("\s","")
-end
+hdest, fsdest, max_age = ARGV[0],ARGV[1],ARGV[2].to_i
+  unless File.exists?(fsdest)
+    puts "Bad directory #{fsdest}!"
+    exit 2
+  end
+  unless max_age > 0
+    puts "Max age must be greater than zero! I don't want to remove ALL your files!"
+    exit 3
+  end
+  def f_age(name)
+    (Time.now - File.ctime(name))/(24*3600)
+  end
+  def tstamp
+    Time.now.strftime('%H:%M__%m-%d-%y').gsub("\s","")
+  end
 while true do
   mtr = `mtr -n -r -c 10 #{hdest}`
   File.open("#{fsdest}#{hdest}__#{tstamp}.txt", 'w') { |f| f.write(mtr)}
+  Dir.chdir(fsdest)
+  Dir.glob('*.txt').each { |filename| File.delete(filename) if f_age(filename) > max_age }
   sleep 60
 end
 
