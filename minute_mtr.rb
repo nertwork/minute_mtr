99c796f2d76e1e30532618b87cd5090ec1bdac44
nertwork <james.farr@dreamhost.com>
2014-10-08 18:01:35 -0700

    Revision 6

--- minute_mtr.rb
+++ minute_mtr.rb
@@ -1,10 +1,16 @@
 #!/usr/bin/ruby
 hdest,fsdest = ARGV
+if ARGV.empty?
+  puts "Improper usage!!"
+  puts "Try minute_mtr.rb DESTINATION FILE_DEST"
+  puts "Example minute_mtr.rb google.com /var/tmp/"
+abort
+end
 def tstamp
   Time.now.strftime('%H:%M__%m-%d-%y').gsub("\s","")
 end
 while true do
-  mtr = `mtr -n -r -c 1 #{hdest}`
+  mtr = `mtr -n -r -c 10 #{hdest}`
   File.open("#{fsdest}#{hdest}__#{tstamp}.txt", 'w') { |f| f.write(mtr)}
   sleep 60
 end
